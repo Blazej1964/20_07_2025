cee03a5525db6d06291fce202f617c92883cb20b
Blazej <brusecki@gmail.com>
2025-09-14 17:32:18 +0200

    koniec

--- sss.py
+++ sss.py
@@ -18,7 +18,7 @@ import pytesseract
 
 env = dotenv_values(".env")
 ### Secrets using Streamlit Cloud Mechanism
-https://docs.streamlit.io/deploy/streamlit-community-cloud/deploy-your-app/secrets-management
+# https://docs.streamlit.io/deploy/streamlit-community-cloud/deploy-your-app/secrets-management
 if 'QDRANT_URL' in st.secrets:
     env['QDRANT_URL'] = st.secrets['QDRANT_URL']
 if 'QDRANT_API_KEY' in st.secrets:
